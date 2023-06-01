43b91a8f1f0c597ec1143fc69d78d11cdbefa3fc
Akash Kumar <46576296+akash1302@users.noreply.github.com>
2023-06-01 10:35:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -182,9 +182,12 @@ resource "aws_instance" "node" {
                   export VAULT_ADDR=http://127.0.0.1:8200
                   export VAULT_SKIP_VERIFY=true
                   consul kv get vault_init.txt > vault_init.txt
-                  UNSEAL_KEY_1=$(cat /home/ubuntu/vault_init.txt | grep "Unseal Key 1:" | awk '{print $NF}')
-                  UNSEAL_KEY_2=$(cat /home/ubuntu/vault_init.txt | grep "Unseal Key 2:" | awk '{print $NF}')
+                  UNSEAL_KEY_1=$(cat vault_init.txt | grep "Unseal Key 1:" | awk '{print $NF}')
+                  UNSEAL_KEY_2=$(cat vault_init.txt | grep "Unseal Key 2:" | awk '{print $NF}')
                   # Unseal Vault with two keys
+                  whoami
+                  pwd
+                  ###########################Hello################
                   vault operator unseal $UNSEAL_KEY_1
                   vault operator unseal $UNSEAL_KEY_2
               else

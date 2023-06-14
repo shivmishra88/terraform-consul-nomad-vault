bcbdc92d12e1c9b650e2d6b4f54bed2519f946b6
Akash Kumar <46576296+akash1302@users.noreply.github.com>
2023-06-15 00:24:59 +0530

    Update ec2.tf

--- Modules/EC2/ec2.tf
+++ Modules/EC2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  count = 7
+  count                  = 7
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
@@ -7,12 +7,14 @@ resource "aws_instance" "ec2" {
   key_name               = var.key_name
   private_ip             = "${var.cnv_ip_range}${count.index+10}"
   tags = {
-        Name = "CNV-${count.index}"
-        Role = count.index < 3 ? "server" : "client"
-    }
-    root_block_device {
+    Name = "CNV-${count.index}"
+    Role = count.index < 3 ? "server" : "client"
+  }
+  root_block_device {
     volume_size = var.cnv_volume_size
     volume_type = "gp3"
-    }
-    user_data = file("${path.module}/../../base_install.sh")
+  }
+  user_data = templatefile("${path.module}/../../base_install.sh", {
+    count = count.index
+  })
 }

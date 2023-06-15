20016fb65214c5d3bbd3f4a305123a30e3e0e958
Akash Kumar <46576296+akash1302@users.noreply.github.com>
2023-06-15 21:22:22 +0530

    Update consul_nomad_vault.tf

--- Stack/consul_nomad_vault.tf
+++ Stack/consul_nomad_vault.tf
@@ -3,7 +3,7 @@
 module "consul_instance" {
     source          = "../Modules/EC2/"  # Replace with the actual path to the module
     name            = "example-instance"
-    ami             = "ami-042e8287309f5df03" # Ubuntu 20.04 LTS
+    ami             = "ami-053b0d53c279acc90" # Ubuntu 22.04 LTS
     instance_type   = "t2.medium"
     subnet_id       = module.aws_vpc.private_subnets[0]
     security_group_ids = [module.aws_vpc.ec2_sg_id]

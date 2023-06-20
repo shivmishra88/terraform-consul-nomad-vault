6a60d4f4a276ab52b4d5c705fa294cbcac702cc7
Akash Kumar <46576296+akash1302@users.noreply.github.com>
2023-06-21 01:41:57 +0530

    Update ec2.tf

--- Modules/EC2/ec2.tf
+++ Modules/EC2/ec2.tf
@@ -18,17 +18,14 @@ user_data = <<-EOF
               #!/bin/bash
               exec > >(tee /home/ubuntu/user_data.log) 2>&1
               sudo hostnamectl set-hostname Node-${count.index}
-              sudo apt-get update -y
-              sudo apt-get install -y unzip jq
-              sudo apt-get install net-tools
-              sudo apt-get install default-jdk -y
+              sudo apt-get update -y && sudo apt-get install -y unzip jq
+              sudo apt-get install net-tools && sudo apt-get install default-jdk -y
               sudo apt-get install bzip2
               private_ip=$(hostname -I | awk '{print $1}')             
               if [ ${count.index} -lt 7 ]; then
               sudo apt install -y docker.io
               echo "${file("${path.module}/../../docker.service.tpl")}" | sudo tee /lib/systemd/system/docker.service
-              sudo systemctl daemon-reload
-              sudo service docker restart
+              sudo systemctl daemon-reload && sudo service docker restart
               fi
               echo "Docker installed"
               sudo useradd --system --home /etc/consul.d --shell /bin/false consul
@@ -53,15 +50,11 @@ user_data = <<-EOF
               sudo systemctl restart consul
               if [ ${count.index} -lt 7 ]; then
               sudo useradd --system --home /etc/nomad.d --shell /bin/false nomad
-              sudo mkdir --parents /etc/nomad.d
-              sudo mkdir --parents /var/nomad
-              sudo chown --recursive nomad:nomad /etc/nomad.d
-              sudo chown --recursive nomad:nomad /var/nomad
+              sudo mkdir --parents /etc/nomad.d && sudo mkdir --parents /var/nomad
+              sudo chown --recursive nomad:nomad /etc/nomad.d && sudo chown --recursive nomad:nomad /var/nomad
               wget https://releases.hashicorp.com/nomad/1.5.5/nomad_1.5.5_linux_amd64.zip
-              unzip nomad_1.5.5_linux_amd64.zip
-              sudo mv nomad /usr/local/bin/
-              sudo chmod 755 /usr/local/bin/nomad
-              sudo chown nomad:nomad /usr/local/bin/nomad 
+              unzip nomad_1.5.5_linux_amd64.zip && sudo mv nomad /usr/local/bin/
+              sudo chmod 755 /usr/local/bin/nomad && sudo chown nomad:nomad /usr/local/bin/nomad 
               else
                   echo "Nomad installed on 7 nodes"
               fi
@@ -73,10 +66,8 @@ user_data = <<-EOF
               elif [ ${count.index} -lt 7 ]; then
                   echo "${file("${path.module}/../../nomad-clients.hcl.tpl")}" | sudo tee /etc/nomad.d/nomad.hcl
                   echo "${file("${path.module}/../../nomad.service")}" | sudo tee /etc/systemd/system/nomad.service
-              sudo systemctl enable nomad
-              sudo systemctl start nomad
-              sudo service consul restart
-              sudo service nomad restart
+              sudo systemctl enable nomad && sudo systemctl start nomad
+              sudo service consul restart && sudo service nomad restart
               fi
               echo "Nomad client and server done"
               if [ ${count.index} -eq 0 ]; then
@@ -102,8 +93,7 @@ user_data = <<-EOF
               elif [ ${count.index} -eq 1 ] || [ ${count.index} -eq 2 ]; then
                   echo "Installing Vault on Node-1 and Node-2..."
                   sudo useradd --system --home /etc/vault.d --shell /bin/false vault
-                  sudo mkdir --parents /etc/vault.d
-                  sudo chown --recursive vault:vault /etc/vault.d
+                  sudo mkdir --parents /etc/vault.d && sudo chown --recursive vault:vault /etc/vault.d
                   curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
                   sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
                   sudo apt-get update && sudo apt-get install vault -y
@@ -125,21 +115,18 @@ user_data = <<-EOF
                   wget https://github.com/contiv/netplugin/releases/download/1.2.0/netplugin-1.2.0.tar.bz2 -P /home/ubuntu
                   sudo tar xvf /home/ubuntu/netplugin-1.2.0.tar.bz2 -C /usr/local/bin/
                   sudo mkdir -p /var/log/contiv/
-                  sudo touch /etc/systemd/system/netmaster.service
-                  sudo touch /etc/systemd/system/netplugin.service
+                  sudo touch /etc/systemd/system/netmaster.service && sudo touch /etc/systemd/system/netplugin.service
                   sudo apt install -y openvswitch-switch
               if [ ${count.index} -eq 0 ] || [ ${count.index} -eq 1 ] || [ ${count.index} -eq 2 ]; then
                   echo "${file("${path.module}/../../netmaster.service.tpl")}" | sudo tee /etc/systemd/system/netmaster.service
                   echo "${file("${path.module}/../../netplugin.service.tpl")}" | sudo tee /etc/systemd/system/netplugin.service
                   sudo service openvswitch-switch restart
                   sudo /usr/share/openvswitch/scripts/ovs-ctl restart
-                  sudo service netmaster restart
-                  sudo service netplugin restart
+                  sudo service netmaster restart && sudo service netplugin restart
               elif [ ${count.index} -eq 3 ] || [ ${count.index} -eq 4 ] || [ ${count.index} -eq 5 ] || [ ${count.index} -eq 6 ]; then
                   echo "${file("${path.module}/../../netplugin.service.tpl")}" | sudo tee /etc/systemd/system/netplugin.service
                   sudo service openvswitch-switch restart
-                  sudo /usr/share/openvswitch/scripts/ovs-ctl restart
-                  sudo service netplugin restart
+                  sudo /usr/share/openvswitch/scripts/ovs-ctl restart && sudo service netplugin restart 
               else
                   echo "Else nothing"
               fi
@@ -147,8 +134,7 @@ user_data = <<-EOF
                if [ ${count.index} -ge 7 ] && [ ${count.index} -le 9 ]; then
                   wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz -P /opt
                   sudo tar -xzf /opt/zookeeper-3.4.9.tar.gz -C /opt
-                  sudo mv /opt/zookeeper-3.4.9 /opt/zookeeper
-                  sudo mkdir -p /var/lib/zookeeper/data
+                  sudo mv /opt/zookeeper-3.4.9 /opt/zookeeper && sudo mkdir -p /var/lib/zookeeper/data
                   sudo cp /opt/zookeeper/conf/zoo_sample.cfg /opt/zookeeper/conf/zoo.cfg
                   echo "${file("${path.module}/../../zoo.cfg.tpl")}" | sudo tee /opt/zookeeper/conf/zoo.cfg
                 if [ ${count.index} -eq 7 ]; then
@@ -170,10 +156,8 @@ user_data = <<-EOF
                   sudo /opt/solr/bin/solr start -c -s  /opt/solr/server/solr -p 8983 -z 10.0.1.17:2181,10.0.1.18:2181,10.0.1.19:2181 -noprompt -force
                   sudo echo "deb https://debian.datastax.com/enterprise/ stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.sources.list
                   sudo curl -L https://debian.datastax.com/debian/repo_key | sudo apt-key add -
-                  sudo apt-get update
-                  sudo apt-get install dse-full -y
-                  sudo systemctl daemon-reload
-                  sudo service dse start
+                  sudo apt-get update && sudo apt-get install dse-full -y
+                  sudo systemctl daemon-reload && sudo service dse start
                   echo "${file("${path.module}/../../cassandra.yaml.tpl")}" | sudo tee /etc/dse/cassandra/cassandra.yaml
                   sudo service dse restart
                else

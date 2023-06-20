2377313397d577a3e4ba161b12a389f1996e1e8d
Akash Kumar <46576296+akash1302@users.noreply.github.com>
2023-06-21 01:31:28 +0530

    Update ec2.tf

--- Modules/EC2/ec2.tf
+++ Modules/EC2/ec2.tf
@@ -8,7 +8,6 @@ resource "aws_instance" "ec2" {
   private_ip             = "${var.cnv_ip_range}${count.index+10}"
   tags = {
         Name = "CNV-${count.index}"
-   #     Role = count.index < 3 ? "server" : "client"
         Role = count.index < 3 ? "server" : count.index < 7 ? "client" : "solr-dse"
     }
     root_block_device {
@@ -24,9 +23,7 @@ user_data = <<-EOF
               sudo apt-get install net-tools
               sudo apt-get install default-jdk -y
               sudo apt-get install bzip2
-              # Get private IP address
               private_ip=$(hostname -I | awk '{print $1}')             
-              ##########Install Docker###
               if [ ${count.index} -lt 7 ]; then
               sudo apt install -y docker.io
               echo "${file("${path.module}/../../docker.service.tpl")}" | sudo tee /lib/systemd/system/docker.service
@@ -34,19 +31,16 @@ user_data = <<-EOF
               sudo service docker restart
               fi
               echo "Docker installed"
-              # Create consul user
               sudo useradd --system --home /etc/consul.d --shell /bin/false consul
               sudo mkdir --parents /etc/consul.d
               sudo mkdir --parents /var/consul
               sudo chown --recursive consul:consul /etc/consul.d
               sudo chown --recursive consul:consul /var/consul
-              # Install Consul
               wget https://releases.hashicorp.com/consul/1.15.2/consul_1.15.2_linux_amd64.zip
               unzip consul_1.15.2_linux_amd64.zip
               sudo mv consul /usr/local/bin/
               sudo chmod 755 /usr/local/bin/consul
               sudo chown consul:consul /usr/local/bin/consul
-              #Consul Template files
               if [ ${count.index} -eq 0 ]; then
                   echo "${file("${path.module}/../../consul-server-bootstrap.hcl.tpl")}" | sudo tee /etc/consul.d/consul.hcl
               elif [ ${count.index} -eq 1 ] || [ ${count.index} -eq 2 ]; then
@@ -55,19 +49,14 @@ user_data = <<-EOF
                   echo "${file("${path.module}/../../consul-client.hcl.tpl")}" | sudo tee /etc/consul.d/consul.hcl
               fi
               echo "${file("${path.module}/../../consul.service")}" | sudo tee /etc/systemd/system/consul.service
-           
-              # Enable and start Consul
               sudo systemctl enable consul
               sudo systemctl restart consul
-
-              # Create nomad user
               if [ ${count.index} -lt 7 ]; then
               sudo useradd --system --home /etc/nomad.d --shell /bin/false nomad
               sudo mkdir --parents /etc/nomad.d
               sudo mkdir --parents /var/nomad
               sudo chown --recursive nomad:nomad /etc/nomad.d
               sudo chown --recursive nomad:nomad /var/nomad
-              # Install Nomad
               wget https://releases.hashicorp.com/nomad/1.5.5/nomad_1.5.5_linux_amd64.zip
               unzip nomad_1.5.5_linux_amd64.zip
               sudo mv nomad /usr/local/bin/
@@ -75,11 +64,8 @@ user_data = <<-EOF
               sudo chown nomad:nomad /usr/local/bin/nomad 
               else
                   echo "Nomad installed on 7 nodes"
-
               fi
               echo "Nomad installed"
-
-              # Copy the Nomad configuration file and systemd service file
               if [ ${count.index} -eq 0 ]; then
                   echo "${file("${path.module}/../../nomad.bootstrap.hcl.tpl")}" | sudo tee /etc/nomad.d/nomad.hcl
               elif [ ${count.index} -eq 1 ] || [ ${count.index} -eq 2 ] ; then
@@ -87,17 +73,14 @@ user_data = <<-EOF
               elif [ ${count.index} -lt 7 ]; then
                   echo "${file("${path.module}/../../nomad-clients.hcl.tpl")}" | sudo tee /etc/nomad.d/nomad.hcl
                   echo "${file("${path.module}/../../nomad.service")}" | sudo tee /etc/systemd/system/nomad.service
-              # Enable and start Nomad
               sudo systemctl enable nomad
               sudo systemctl start nomad
               sudo service consul restart
               sudo service nomad restart
               fi
               echo "Nomad client and server done"
-              # Install Vault
               if [ ${count.index} -eq 0 ]; then
                   echo "Installing Vault on Node-0..."
-                  # Create vault user
                   sudo useradd --system --home /etc/vault.d --shell /bin/false vault
                   sudo mkdir --parents /etc/vault.d
                   sudo chown --recursive vault:vault /etc/vault.d
@@ -112,15 +95,12 @@ user_data = <<-EOF
                   UNSEAL_KEY_1=$(cat /home/ubuntu/vault_init.txt | grep "Unseal Key 1:" | awk '{print $NF}')
                   UNSEAL_KEY_2=$(cat /home/ubuntu/vault_init.txt | grep "Unseal Key 2:" | awk '{print $NF}')
                   ROOT_TOKEN=$(cat /home/ubuntu/vault_init.txt | grep "Initial Root Token:" | awk '{print $NF}')
-                  # Unseal Vault with two keys
                   vault operator unseal $UNSEAL_KEY_1
                   vault operator unseal $UNSEAL_KEY_2
                   cd /home/ubuntu
                   consul kv put vault_init.txt @/home/ubuntu/vault_init.txt
-              ##########################Node-1 and Node-2####
               elif [ ${count.index} -eq 1 ] || [ ${count.index} -eq 2 ]; then
                   echo "Installing Vault on Node-1 and Node-2..."
-                  #####
                   sudo useradd --system --home /etc/vault.d --shell /bin/false vault
                   sudo mkdir --parents /etc/vault.d
                   sudo chown --recursive vault:vault /etc/vault.d
@@ -136,14 +116,12 @@ user_data = <<-EOF
                   export VAULT_SKIP_VERIFY=true
                   UNSEAL_KEY_1=$(cat /home/ubuntu/vault_init.txt | grep "Unseal Key 1:" | awk '{print $NF}')
                   UNSEAL_KEY_2=$(cat /home/ubuntu/vault_init.txt | grep "Unseal Key 2:" | awk '{print $NF}')
-                  # Unseal Vault with two keys
                   vault operator unseal $UNSEAL_KEY_1
                   vault operator unseal $UNSEAL_KEY_2
               else
                   echo "Else nothing"
               fi
                   echo "Installation has been done"
-               #################################################Contiv########
                   wget https://github.com/contiv/netplugin/releases/download/1.2.0/netplugin-1.2.0.tar.bz2 -P /home/ubuntu
                   sudo tar xvf /home/ubuntu/netplugin-1.2.0.tar.bz2 -C /usr/local/bin/
                   sudo mkdir -p /var/log/contiv/
@@ -166,7 +144,6 @@ user_data = <<-EOF
                   echo "Else nothing"
               fi
                   echo "Installation has been done"
-              #########################################Solr and Zk##########################
                if [ ${count.index} -ge 7 ] && [ ${count.index} -le 9 ]; then
                   wget https://archive.apache.org/dist/zookeeper/zookeeper-3.4.9/zookeeper-3.4.9.tar.gz -P /opt
                   sudo tar -xzf /opt/zookeeper-3.4.9.tar.gz -C /opt
@@ -174,7 +151,6 @@ user_data = <<-EOF
                   sudo mkdir -p /var/lib/zookeeper/data
                   sudo cp /opt/zookeeper/conf/zoo_sample.cfg /opt/zookeeper/conf/zoo.cfg
                   echo "${file("${path.module}/../../zoo.cfg.tpl")}" | sudo tee /opt/zookeeper/conf/zoo.cfg
-                  
                 if [ ${count.index} -eq 7 ]; then
                   sudo echo "1" > /var/lib/zookeeper/data/myid
                 elif [ ${count.index} -eq 8 ]; then
@@ -182,7 +158,6 @@ user_data = <<-EOF
                 elif [ ${count.index} -eq 9 ]; then
                   sudo echo "3" > /var/lib/zookeeper/data/myid
                 fi
-
                 if [ ${count.index} -ge 7 ] && [ ${count.index} -le 9 ]; then
                   sudo /opt/zookeeper/bin/zkServer.sh start
                 fi
